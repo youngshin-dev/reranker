77b4f2ef84e0489ac4995bacf0e2ca6c01a0e19f
YOUNGSHIN OH <youngshin.dev@gmail.com>
2016-11-25 14:42:11 -0800

    changed the generator into lists at the cost of speed. Need a fix to use generator for bleu score

--- learn.py
+++ learn.py
@@ -5,8 +5,8 @@ from collections import defaultdict
 import bleu
 import random
 import math
-from numpy import array, dot, random
-
+import numpy
+from operator import itemgetter
 
 
 optparser = optparse.OptionParser()
@@ -17,60 +17,67 @@ epoches=5
 tau = 500
 alpha=0.1
 xi=100
-w = random.rand(6)
+theta = numpy.random.rand(6)
 eta=0.1
 
 (opts, _) = optparser.parse_args()
-#translations={}
-translations=defaultdict(list)
-#all_translations=[]
-#translations=[]
+
 source = open(opts.ref).read().splitlines()
-a_translation=namedtuple('a_translation','sentence, features, stats')
+a_translation=namedtuple('a_translation','sentence, features, smoothed_bleu')
 nbests = [[] for i in range(len(source))]
+
+
 for line in open(opts.nbest):
     (i, sentence, features) = line.strip().split("|||")
     ind=int(i)
-    bleu_stat=bleu.bleu_stats(sentence, source[ind])
-    #test=list(bleu_stat)[:2]
+    #stats=bleu.bleu_stats(sentence, source[ind])
+    stats=list(bleu.bleu_stats(sentence, source[ind]))
     #test1=test[0]
-    bleu_score=bleu.bleu(bleu_stat)
-
-
-    nbests[ind].append(a_translation(sentence,features,bleu.bleu_stats(sentence, source[ind])))
+    bleu_smooth_score=bleu.smoothed_bleu(stats)
+    feature_vec=numpy.fromstring(features, sep=' ')
+    nbests[ind].append(a_translation(sentence,feature_vec,bleu_smooth_score))
 
 def get_sample(nbest):
     sample=[]
     for i in range(0,tau):
-        random_items = random.sample(nbest, 2)
-        s1 = random_items[0]
-        s2 = random_items[1]
+        #random_items = random.sample(nbest, 2)
+        #s1 = random_items[0]
+        #s2 = random_items[1]
+        s1=random.choice(nbest) 
+        s2=random.choice(nbest) 
         if math.fabs(s1.smoothed_bleu - s2.smoothed_bleu) > alpha:
             if s1.smoothed_bleu > s2.smoothed_bleu:
-                sample += (s1, s2)
+                #tuple1=(s1, s2)
+                sample.append([s1,s2])
             else:
-                sample += (s2, s1)
+                #tuple2 = (s2 ,s1)
+                sample.append([s2,s1])
         else:
             continue
     return sample
 
 for i in range(0,epoches):
     for nbest in nbests:
-        #random_items = random.sample(nbest, 2)
-        #s1 = random_items[0]
-        #s2 = random_items[1]
-        s=nbest[4]
-        test=s.stats.smoothed_bleu
-        '''
-        sample=get_sample(nbest).sort(key=lambda tup:(math.fabs(tup[0]-tup[1])))[:xi]
+
+        sample=get_sample(nbest)
+        #sorted_sample=sample.sort(key=lambda tup:(math.fabs(tup[0].smoothed_bleu-tup[1].smoothed_bleu)))[:xi]
+        sorted_sample=sorted(sample,key=lambda x: math.fabs(x[0].smoothed_bleu-x[1].smoothed_bleu))
         mistakes=0
-        for item in sample:
-            if dot(w,item[1])<=dot(w,item[0]):
+        for item in sorted_sample:
+            feature1=item[0].features
+            feature2=item[1].features
+            if numpy.dot(theta,feature1)<=numpy.dot(theta,feature2):
                 mistakes=mistakes+1
-                w=eta*()
-        '''
+                theta=theta+eta*(feature1-feature2)
+            else:
+                mistakes = mistakes + 1
+                theta = theta + eta * (feature2 - feature1)
 
+def main():
+    print "\n".join([str(weight) for weight in theta])
 
+if __name__ == "__main__":
+    main()
 #features = [float(h) for h in features.strip().split()]
 #w = [1.0/len(features) for _ in xrange(len(features))]
 #break
